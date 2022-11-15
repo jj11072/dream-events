ef29ca25129e768b96f97293cc2f46e92bd7e767
jj1107 <justinjohnson1234@msn.com>
2022-11-15 20:26:54 +0000

    hero

--- components/hero.js
+++ components/hero.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const hero = () => {
   return (
-    <section className="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat border border-neutral-300">
-      <div className="absolute inset-0 dark:bg-black/75 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r dark:sm:from-black/80 sm:from-white/95 sm:to-white/25 dark:sm:to-black/0"></div>
+    <section className="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat ">
+      <div className="absolute inset-0 dark:bg-black/25 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r dark:sm:from-black/100 sm:from-white/95 sm:to-white/25 dark:sm:to-black/0"></div>
 
       <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
         <div className="max-w-xl text-center sm:text-left">
@@ -24,7 +24,7 @@ const hero = () => {
             <a
               href="/contact"
               className="block w-full rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-pink-700 focus:outline-none focus:ring active:bg-rose-pink sm:w-auto">
-              Book Now
+              Book Event 
             </a>
 
             <a
